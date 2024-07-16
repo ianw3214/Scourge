251d4e5d8817c73eb0cba4e821fe6934ab63fb15
ianw3214 <ianw3214@gmail.com>
2024-07-16 01:11:23 -0700

    Added JSON file loading to engine

--- shade/src/shade/file/fileSystem.h
+++ shade/src/shade/file/fileSystem.h
@@ -3,18 +3,24 @@
 #include <memory>
 #include <string>
 
+#include "shade/file/jsonFile.h"
 #include "shade/file/textFile.h"
 #include "shade/file/keyValueFile.h"
 #include "shade/instance/service/service.h"
 
 namespace Shade {
 
+    // The file system is a centralized service place to load/save files
+    //  - Files typically are read and then discarded
+    //  - As such, there is no mechanism for retaining file resources provided by the system
+    //  - Ownership is transferred to the caller when loading a file
     class FileSystem : public Service {
     public:
         FileSystem();
 
         std::unique_ptr<TextFile> LoadTextFile(const std::string& path);
         std::unique_ptr<KeyValueFile> LoadKeyValueFile(const std::string& path);
+        std::unique_ptr<JsonFile> LoadJsonFile(const std::string& path);
 
         bool SaveKeyValueFile(const std::string& path, const KeyValueFile& fileData);
     };
